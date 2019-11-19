b1b79c0d6d76256056484babc88013ea9c68f5ec
NickPtr <44138348+NickPtr@users.noreply.github.com>
2019-11-19 14:00:11 +0200

    first_step

---
+++ defibrillator/defibrillator/LogIn.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace defibrillator
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LogIn : ContentPage
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Clicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Create_OnClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
