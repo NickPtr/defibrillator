0e0342d9be274fc2ccd9abdd3f6fe780c67071f6
NickPtr <44138348+NickPtr@users.noreply.github.com>
2019-11-20 19:04:49 +0200

    uploadPhotoWork

--- defibrillator/defibrillator/Views/Camera.xaml.cs
+++ defibrillator/defibrillator/Views/Camera.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.Storage;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ namespace defibrillator.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Camera : ContentPage
     {
+        private Stream camerasStream;
         public Camera()
         {
             InitializeComponent();
@@ -26,7 +29,31 @@ namespace defibrillator.Views
             var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
 
             if (photo != null)
-                PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+                PhotoImage.Source = ImageSource.FromStream(() =>
+                {
+                    camerasStream = photo.GetStream();
+                    return photo.GetStream();
+                });
+        }
+
+
+        private async void UploadImage(Stream stream)
+        {
+            var account = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=defibrillatorphotodata;AccountKey=d7CG+7nZGxUc40BPZXSLVi1eILcwEd3wb/JyrMh/AlL+kp/jjFGhW2vZawUQEpRDTujlJJ0hrStaM7n4We2vWw==;EndpointSuffix=core.windows.net");
+            var client = account.CreateCloudBlobClient();
+            var container = client.GetContainerReference("images");
+            await container.CreateIfNotExistsAsync();
+            var name = Guid.NewGuid().ToString();
+            var blockBlob = container.GetBlockBlobReference($"{name}.png");
+            await blockBlob.UploadFromStreamAsync(stream);
+            string URL = blockBlob.Uri.OriginalString;
+           
+            await DisplayAlert("Uploaded", "Image uploaded to Blob Storage Successfully!", "OK");
+        }
+
+        private void Save_OnClicked(object sender, EventArgs e)
+        {
+           UploadImage(camerasStream);
         }
     }
 }
